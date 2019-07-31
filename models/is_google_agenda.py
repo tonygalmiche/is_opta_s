d49b7e916419e17f8582d18c72f10425a195a34f
Tony Galmiche <tony.galmiche@infosaone.com>
2019-07-31 15:00:47 +0200

    add google agenda

--- models/is_google_agenda.py
+++ models/is_google_agenda.py
@@ -8,7 +8,7 @@ class IsGoogleAgendaCategorie(models.Model):
     _description = "Catégorie Google Agenda"
     _order='name'
 
-    name = fields.Char(u"Catégorie Google Agenda")
+    name = fields.Char(u"Catégorie Google Agenda",required=1)
 
 
 class IsGoogleAgenda(models.Model):
@@ -16,12 +16,28 @@ class IsGoogleAgenda(models.Model):
     _description = "Evénements Google Agenda "
     _order='date_debut desc'
 
-    user_id      = fields.Many2one('res.users', u'Utilisateur')
-    date_debut   = fields.Datetime(u"Date début")
-    date_fin     = fields.Datetime(u"Date fin")
-    duree        = fields.Float(u"Durée")
-    event_id     = fields.Char(u"Id Event Google Agenda")
-    intitule     = fields.Char(u"Intitulé")
-    categorie_id = fields.Many2one('is.google.agenda.categorie', u'Catégorie')
+    user_id          = fields.Many2one('res.users', u'Utilisateur',index=1)
+    date_debut       = fields.Datetime(u"Date début",index=1)
+    date_fin         = fields.Datetime(u"Date fin",index=1)
+    duree            = fields.Float(u"Durée")
+    temps_disponible = fields.Boolean(u"Temps disponible",index=1,default=False)
+    event_id         = fields.Char(u"Id Event Google Agenda",index=1)
+    intitule         = fields.Char(u"Intitulé",index=1)
+    categorie_id     = fields.Many2one('is.google.agenda.categorie', u'Catégorie',index=1)
 
+
+
+
+    @api.model
+    def actualiser_action(self, *args, **kwargs):
+        if not self.env.user._is_admin():
+            raise AccessDenied()
+
+        print('#### actualiser_action')
+        self.env['res.users'].search([]).actualiser_google_agenda_action()
+
+        #self.env['ir.attachment']._file_gc()
+        #self._gc_transient_models()
+        #self._gc_user_logs()
+        return True
 
--- models/res_users.py
+++ models/res_users.py
@@ -3,12 +3,9 @@ from odoo import api, fields, models, _
 import recurring_ical_events
 import urllib.request
 import icalendar
-
-
-#TODO : Mettre l'heure en UTC dans Odoo
-#Supprimer ou mettre à jour les lignes => Surement plus rapide de tout supprimer
-#Retrouer les bonnes catéroeies et ajouter le temps restant dans une journée
-
+from datetime import datetime,timedelta,date
+import logging
+_logger = logging.getLogger(__name__)
 
 
 class IsDynacase(models.Model):
@@ -31,12 +28,11 @@ class ResUsers(models.Model):
             if obj.is_salaire_an>0 and obj.is_nb_jours>0:
                 is_salaire_jour = float(obj.is_salaire_an/obj.is_nb_jours)
             obj.is_salaire_jour=is_salaire_jour
-            print(obj,is_salaire_jour)
 
 
     is_initiales          = fields.Char(u'Initiales pour Chrono')
     is_compte_general     = fields.Char(u'Compte général')
-    is_google_agenda_ical = fields.Char(u'Adresse privée iCal Google Agenda')
+    is_google_agenda_ical = fields.Text(u'Adresse privée iCal Google Agenda')
     is_dynacase_ids       = fields.Many2many('is.dynacase', 'res_users_dynacase_rel', 'doc_id', 'dynacase_id', 'Ids Dynacase', readonly=True)
     is_nb_jours           = fields.Integer('Nombre de jours travaillables par an')
     is_salaire_an         = fields.Integer('Salaire brut chargé par an')
@@ -52,110 +48,130 @@ class ResUsers(models.Model):
 
     @api.multi
     def actualiser_google_agenda_action(self):
+        cr = self._cr
+        _logger.info(u"Début du traitement actualiser_google_agenda_action")
+        categories = self.env['is.google.agenda.categorie'].search([])
+        now = date.today()
+        start_date = now - timedelta(days=31) # 1 mois avant
+        end_date   = now + timedelta(days=92) # 3 mois après
         for obj in self:
+            filtre=[
+                ('date_debut','>=',start_date),
+                ('date_fin','<=',end_date),
+                ('user_id','=',obj.id),
+            ]
+            self.env['is.google.agenda'].search(filtre).unlink()
             url = obj.is_google_agenda_ical
             if url:
-                print(obj,url)
-                filename1 = '/tmp/google-agenda-'+str(obj.id)+'.tmp'
-                filename2 = '/tmp/google-agenda-'+str(obj.id)+'.ics'
-                urllib.request.urlretrieve(url, filename1)
-                f1 = open(filename1)
-                f2 = open(filename2,'w')
-                lines = f1.readlines()
-                line2=''
-                for line in lines:
-                    line2 = line
+                urls=url.split('\n')
+                for url in urls:
+                    _logger.info(obj.name+u' / '+url)
 
-
-                    if line[:6] == 'RRULE:':
-                        t = line.split(';')
-                        line2=[]
-                        for x in t:
-                            if x[:6] == 'UNTIL=':
-                                #print(line.strip(),x.strip())
-                                if x.strip()[-1:]!='Z':
-                                    x = x.strip()[:14]+'T060000Z\n'
-                                    #print('v=',v)
-                            line2.append(x)
-                        line2=';'.join(line2)
-                        print('-',line.strip())
-                        print('*',line2.strip())
-
-
-                    if line[:6] == 'DTEND;':
-                        t = line.split(';')
-                        line2=[]
-                        for x in t:
-                            if x[:11] == 'VALUE=DATE:':
-                                #print(line.strip(),x.strip())
-                                #DTEND;VALUE=DATE:20100207
-                                if x.strip()[-1:]!='Z':
-                                    x = x.strip()[:19]+'T060000Z\n'
-                                    #print('v=',v)
-                            line2.append(x)
-                        line2=';'.join(line2)
-
-
-                    #DTSTART;VALUE=DATE:20120119
-                    if line[:8] == 'DTSTART;':
-                        t = line.split(';')
-                        line2=[]
-                        for x in t:
-                            if x[:11] == 'VALUE=DATE:':
-                                #print(line.strip(),x.strip())
-                                #DTEND;VALUE=DATE:20100207
-                                if x.strip()[-1:]!='Z':
-                                    x = x.strip()[:19]+'T060000Z\n'
-                                    #print('v=',v)
-                            line2.append(x)
-                        line2=';'.join(line2)
-
-
-
-
-                    f2.write(line2)
-                f1.close()
-                f2.close()
-                url = 'file://'+filename2
-                start_date = (2019, 7, 1)
-                end_date =   (2019, 9, 30)
-                ical_string = urllib.request.urlopen(url).read()
-                calendar = icalendar.Calendar.from_ical(ical_string)
-                events = recurring_ical_events.of(calendar).between(start_date, end_date)
-                for event in events:
-                    start = event["DTSTART"].dt
-                    duration = event["DTEND"].dt - event["DTSTART"].dt
-                    summary = event["SUMMARY"]
-                    #print("start {} duration {}".format(start, duration), summary, event["UID"])
-
+                    filename1 = '/tmp/google-agenda-'+str(obj.id)+'.tmp'
+                    filename2 = '/tmp/google-agenda-'+str(obj.id)+'.ics'
+                    urllib.request.urlretrieve(url, filename1)
+                    f1 = open(filename1)
+                    f2 = open(filename2,'w')
+                    lines = f1.readlines()
+                    line2=''
+                    for line in lines:
+                        line2 = line
+                        if line[:6] == 'RRULE:':
+                            t = line.split(';')
+                            line2=[]
+                            for x in t:
+                                if x[:6] == 'UNTIL=':
+                                    if x.strip()[-1:]!='Z':
+                                        x = x.strip()[:14]+'T060000Z\n'
+                                line2.append(x)
+                            line2=';'.join(line2)
+                        if line[:6] == 'DTEND;':
+                            t = line.split(';')
+                            line2=[]
+                            for x in t:
+                                if x[:11] == 'VALUE=DATE:':
+                                    if x.strip()[-1:]!='Z':
+                                        x = x.strip()[:19]+'T060000Z\n'
+                                line2.append(x)
+                            line2=';'.join(line2)
+                        if line[:8] == 'DTSTART;':
+                            t = line.split(';')
+                            line2=[]
+                            for x in t:
+                                if x[:11] == 'VALUE=DATE:':
+                                    if x.strip()[-1:]!='Z':
+                                        x = x.strip()[:19]+'T060000Z\n'
+                                line2.append(x)
+                            line2=';'.join(line2)
+                        f2.write(line2)
+                    f1.close()
+                    f2.close()
+                    url = 'file://'+filename2
+                    ical_string = urllib.request.urlopen(url).read()
+                    calendar = icalendar.Calendar.from_ical(ical_string)
+                    events = recurring_ical_events.of(calendar).between(start_date, end_date)
+                    for event in events:
+                        start = event["DTSTART"].dt
+                        duration = event["DTEND"].dt - event["DTSTART"].dt
+                        summary = event["SUMMARY"]
+                        date_debut = event["DTSTART"].dt
+                        date_fin   =  event["DTEND"].dt
+                        offset = -int(int(date_debut.strftime('%z'))/100)
+                        date_debut_offset = date_debut + timedelta(hours=offset)
+                        date_fin_offset   = date_fin   + timedelta(hours=offset)
+                        duree = date_fin - date_debut
+                        duree = duree.total_seconds()/3600
+                        intitule = str(event["SUMMARY"])
+                        categorie_id = False
+                        for categorie in categories:
+                            lg = len(categorie.name)
+                            if intitule[:lg] == categorie.name:
+                                categorie_id = categorie.id
+                        if not categorie_id:
+                            intitule=False
+                        vals={
+                            'user_id'         : obj.id,
+                            'date_debut'      : date_debut_offset,
+                            'date_fin'        : date_fin_offset,
+                            'duree'           : duree,
+                            'temps_disponible': False,
+                            'event_id'        : str(event["UID"]),
+                            'intitule'        : intitule,
+                            'categorie_id'    : categorie_id,
+                        }
+                        doc=self.env['is.google.agenda'].create(vals)
+        for obj in self:
+            date_debut = start_date
+            for i in range(1,125):
+                if int(date_debut.strftime('%w'))!=0 and int(date_debut.strftime('%w'))!=6:
+                    SQL="""
+                        select to_char(date_debut,'YYYY-MM-DD'),sum(duree)
+                        from is_google_agenda
+                        where 
+                            user_id="""+str(obj.id)+""" and
+                            date_debut>='"""+str(date_debut)+""" 00:00:00' and
+                            date_debut<='"""+str(date_debut)+""" 23:59:59'
+                        group by to_char(date_debut,'YYYY-MM-DD')
+                        order by to_char(date_debut,'YYYY-MM-DD')
+                    """
+                    cr.execute(SQL)
+                    result = cr.fetchall()
+                    temps_libre = 7
+                    for row in result:
+                        temps_libre = 7 - row[1]
+                        if temps_libre<0:
+                            temps_libre=0
                     vals={
-                        'user_id'     : obj.id,
-                        'date_debut'  : event["DTSTART"].dt,
-                        'date_fin'    : event["DTEND"].dt,
-                        #'duree'       : duration,
-                        'event_id'    : str(event["UID"]),
-                        'intitule'    : str(event["SUMMARY"]),
-                        'categorie_id': False,
+                        'user_id'         : obj.id,
+                        'date_debut'      : str(date_debut)+' 12:00:00',
+                        'date_fin'        : str(date_debut)+' 12:00:00',
+                        'duree'           : temps_libre,
+                        'temps_disponible': True,
+                        'event_id'        : False,
+                        'intitule'        : 'Temps Disponible',
+                        'categorie_id'    : False,
                     }
-                    #print(vals)
                     doc=self.env['is.google.agenda'].create(vals)
-
-
-#    user_id      = fields.Many2one('res.users', u'Utilisateur')
-#    date_debut   = fields.Datetime(u"Date début")
-#    date_fin     = fields.Datetime(u"Date fin")
-#    duree        = fields.Float(u"Durée")
-#    event_id     = fields.Char(u"Id Event Google Agenda")
-#    intitule     = fields.Char(u"Intitulé")
-#    categorie_id = fields.Many2one('is.google.agenda.categorie', u'Catégorie')
-
-
-
-
-
-
-
-
-
-
+                date_debut = date_debut + timedelta(days=1)
+        _logger.info(u"Fin du traitement actualiser_google_agenda_action")
 
